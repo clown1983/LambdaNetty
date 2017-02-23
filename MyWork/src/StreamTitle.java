18264534adca90dce90e004e45d28ebaee9f0a70
clown1983 <clown1983@daum.net>
2017-02-23 11:40:49 +0900

    테스트

--- MyWork/src/StreamTitle.java
+++ MyWork/src/StreamTitle.java
@@ -7,14 +7,16 @@ import java.util.stream.Stream;
 
 public class StreamTitle {
   public static void main(String[] args) {
-//    List<String> title = Arrays.asList("JAVA","TEST","AAA");
-//    Stream<String> s = title.stream();
-//    s.forEach(System.out::println);
+    List<String> title = Arrays.asList("JAVA","TEST","AAA");
+    Stream<String> s = title.stream();
+    s.forEach(System.out::println);
         
 //    ArrayList<String> a = new ArrayList<>();
 //    a.add("aaa");
 //    List<Object> parts = Arrays.asList(a.toArray());
     IntStream.range(0, 10).forEach(System.out::println);
+    
+    
   }
   
   public static void test(String a){
